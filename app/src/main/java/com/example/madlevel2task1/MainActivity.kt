11b0f7c17ba8d4ddeb1d1475bcc31c61fe8be7e1
ArieBisfki <arie_bisfki@live.nl>
2020-09-13 13:30:08 +0200

    Connected PlaceAdapter to recyclerView. Assignment completed.

--- app/src/main/java/com/example/madlevel2task1/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.madlevel2task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.madlevel2task1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews()
+    }
+
+    private fun initViews() {
+        val places = arrayListOf<Place>()
+        for (i in Place.PLACE_NAMES.indices) {
+            val place = Place(Place.PLACE_NAMES[i], Place.PLACE_RES_DRAWABLE_IDS[i])
+            places.add(place)
+        }
+
+        binding.rvPlaces.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        binding.rvPlaces.adapter = PlaceAdapter(places)
     }
 }
